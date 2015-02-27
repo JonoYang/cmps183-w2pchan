cabdee13197e7116c025a5146708e0dfaf621aa9
Steven Esser <steven.michael.esser@gmail.com>
2015-02-26 19:13:40 -0800

    updated tables.py

--- models/tables.py
+++ models/tables.py
@@ -5,21 +5,17 @@ from gluon import *
 def ip():
     return current.request.client
 
-db.define_table('post',
-                Field('author'),
-                Field('author_ip', writable = False, readable = False, 
-                        default = ip()),
-                Field('date_created', 'datetime', writable = False,
-                        default = datetime.utcnow()),
-                Field('image', 'upload'),
-                Field('body', 'text')
-                )
+db.define_table('board', 
+                Field('name'))
 
 db.define_table('thread',
-                Field('author'),
-                Field('author_ip', writable = False, readable = False,
-                        default = ip()),
-                Field('date_created', 'datetime', writable = False,
-                        default = datetime.utcnow()),
-                Field('posts', db.post)
-                )
+                Field('title'),
+		Field('date_created', 'datetime', default = datetime.utcnow()),
+		Field('board_id', db.board))
+
+db.define_table('post',
+                Field('body'),
+		Field('author'),
+		Field('date_created', 'datetime', default = datetime.utcnow()),
+		Field('image', 'upload'),
+		Field('thread_id', db.thread))
