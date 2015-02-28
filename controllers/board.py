6135acac11232eec9faf389b4c598a52575f3273
Steven Esser <steven.michael.esser@gmail.com>
2015-02-28 13:23:57 -0800

    board name now showing up on board/index

--- controllers/board.py
+++ controllers/board.py
@@ -1,4 +1,4 @@
 def index():
-    board_id = request.args(0)
-    threads = db(db.thread.board_id == board_id).select(orderby =~ db.thread.date_created)
-    return dict(threads = threads, board_id = board_id)
+    board = db.board[request.args(0)]
+    threads = db(db.thread.board_id == board.id).select(orderby =~ db.thread.date_created)
+    return dict(threads = threads, board = board)
