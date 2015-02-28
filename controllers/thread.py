b03796db03d47045bfaa85eb9ed0ad487cbbe351
Jono Yang <the.jonathan.yang@gmail.com>
2015-02-28 14:59:29 -0800

    fixing upload form to stop making duplicate entries in db

--- controllers/thread.py
+++ controllers/thread.py
@@ -3,7 +3,10 @@ def index():
     posts = db(db.post.thread_id == thread_id).select(orderby =~ db.post.date_created)
     thread = db.thread[request.args(0)]
 
-    form = SQLFORM(db.post)
+    form = SQLFORM.factory(Field('author'),
+                            Field('image', 'upload', uploadfolder = 'applications/w2pchan/uploads'),
+                            Field('body', 'text'),
+                            table_name = 'post')
     if form.process().accepted:
         db.post.insert(thread_id = thread_id, author = form.vars.author, 
                         body = form.vars.body, image = form.vars.image)
