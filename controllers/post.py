d1a3746ad288e85ddb8d94d22471e6ff439a3507
Jono Yang <the.jonathan.yang@gmail.com>
2015-03-03 17:20:18 -0800

    hot sex

--- controllers/post.py
+++ controllers/post.py
@@ -10,5 +10,14 @@ def new():
 @auth.requires_login()
 def delete():
     post = db.post(request.args(0))
+    thread_id = post.thread_id
+    confirm = ''
     if post.author_id != auth.user_id:
-    	redirect(URL(''))
+        session.flash = 'Not authorized, BITCH'
+        redirect(URL('thread', 'index', args = [thread_id]))
+    else:
+        confirm = FORM.confirm('DO YOU WANT TO DELETE THIS, BITCH')
+        if confirm.accepted:
+            db(db.post.id == post.id).delete()
+            redirect(URL('thread', 'index', args = [thread_id]))
+    return dict(confirm = confirm)
