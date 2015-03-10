5ea9f91ed1c67359523fdb7458afddd1b8e850b6
Jono Yang <the.jonathan.yang@gmail.com>
2015-03-09 21:42:24 -0700

    image required to make thread

--- controllers/board.py
+++ controllers/board.py
@@ -5,7 +5,6 @@ def index():
     board_name = db.board[board_id].name
     threads = db(db.thread.board_id == board_id).select(orderby =~ db.thread.date_updated)
     posts = db(db.post).select()
-    print posts
     form = SQLFORM.factory(Field('title'),
                         Field('author'),
                         Field('image', 'upload', uploadfolder = 'applications/w2pchan/uploads'),
@@ -13,13 +12,16 @@ def index():
                         table_name = 'thread')
     if form.process().accepted:
         if auth.user:
-            board_id = request.args(0)
-            db.thread.insert(author = form.vars.author, board_id = board_id,
+            if form.vars.image == '':
+                response.flash = 'Image requred'
+            else:
+                board_id = request.args(0)
+                db.thread.insert(author = form.vars.author, board_id = board_id,
                                 title = form.vars.title, date_updated = datetime.utcnow(),
                                 image = form.vars.image,
                                 body = form.vars.body)
-            shit =  db(db.thread).select().last().id
-            redirect(URL('thread', 'index', args=[shit]))
+                shit =  db(db.thread).select().last().id
+                redirect(URL('thread', 'index', args=[shit]))               
         else:
             response.flash = 'Not logged in'
     return dict(threads = threads, board_id = board_id, board_name = board_name, form = form, posts = posts)
